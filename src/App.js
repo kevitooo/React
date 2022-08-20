7a492035df021ee6e407a838356a62ee70e23d3c
kevitooo <kevinschafersoftware@gmail.com>
2022-08-20 15:12:57 -0300

    Adding a new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './screens/Home';
+import About from './screens/About';
+import Contact from './screens/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar></NavBar>
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='/about' element={<About/>}></Route>
+          <Route exact path='/contact' element={<Contact/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
